72ef83a437d2cd37c61dd748fec5431fef43a6f0
Max Turing Linus Sinoh <121413904+MaxSinoh@users.noreply.github.com>
2025-01-15 19:24:57 +0800

    MythOS Header Files

---
+++ includes/gui/color.h
@@ -0,0 +1,51 @@
+//
+//                       _oo0oo_
+//                      o8888888o
+//                      88' . '88
+//                      (| -_- |)
+//                      0\  =  /0
+//                    ___/`---'\___
+//                  .' \|     |// '.
+//                 / \|||  :  |||// "
+//                / _||||| -:- |||||- "
+//               |   | \\  -  /// |   |
+//               | \_|  ''\---/''  |_/ |
+//               \  .-\__  '-'  ___/-. /
+//             ___'. .'  /--.--\  `. .'___
+//          .''' '<  `.___\_<|>_/___.' >' '''.
+//          | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+//          \  \ `_.   \_ __\ /__ _/   .-` /  /
+//      =====`-.____`.___ \_____/___.-`___.-'=====
+//                        `=---='
+//
+//      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//             ***GOD BLESS   ·   NEVER CRASH***
+//
+//      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//        COPYRIGHT (C) 2025 MAXSINOH & LUOYUDIAN
+//                  ALL RIGHTS RESERVED
+//
+//      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//                MYTHOS COLOR HEADER FILE
+//
+//      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#ifndef _COLOR_H_
+#define _COLOR_H_
+
+#include "graphics.h"
+
+PixelColor darkLightGray = {192, 192, 192}; // A darker light gray.
+PixelColor lightDarkGray = {128, 128, 128}; // A light dark gray.
+PixelColor black = {0, 0, 0};               // Black.
+PixelColor white = {255, 255, 255};         // White.
+PixelColor red = {255, 0, 0};               // Red.
+PixelColor green = {0, 255, 0};             // Green.
+PixelColor blue = {0, 0, 255};              // Blue.
+PixelColor yellow = {255, 255, 0};          // Yellow.
+PixelColor cyan = {0, 255, 255};            // Cyan.
+
+#endif
